82056792a7deea818d5894a1e00e2c0f8c51c50a
Rajvardhan <singhrajvardhan2003@gmail.com>
2023-01-13 12:59:17 +0530

    working on navbar configs

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -1,5 +1,73 @@
+import { useState } from "react";
+import {
+    Box,
+    IconButton,
+    InputBase,
+    Typography,
+    Select,
+    MenuItem,
+    FormControl,
+    useTheme,
+    useMediaQuery
+} from "@mui/material";
+import {
+    Search,
+    Message,
+    DarkMode,
+    LightMode,
+    Notifications,
+    Help,
+    Menu,
+    Close
+} from "@mui/icons-material";
+import { useDispatch, useSelector } from "react-redux";
+import { setMode, setLogout } from "state";
+import { useNavigate } from "react-router-dom";
+import { FlexBetween } from "components/FlexBetween";
+
+
 const Navbar = () => {
-    return (<div>Navbar</div>)
+    const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const user = useSelector((state) => state.user);
+    const isNonMobileScreens = useMediaQuery("(min-width:100px");
+
+    const theme = useTheme();
+    const neutralLight = theme.palette.neutral.light;
+    const dark = theme.palette.neutral.dark;
+    const background = theme.palette.background.default;
+    const primaryLight = theme.palette.primary.light;
+    const alt = theme.palette.background.alt;
+
+    const fullName = `${user.firstName} ${user.lastName}`;
+
+    return <FlexBetween padding="1rem 6%" backgroundColor={alt}>
+        <FlexBetween gap="1.75rem">
+            <Typography fontWeight="bold"
+                fontSize="clamp(1rem 2rem 2.25rem)"
+                color="primary"
+                onClick={() => navigate("/home")}
+                sx={{
+                    "&:hover": {
+                        color: primaryLight,
+                        cursor: "pointer",
+                    },
+                }}>
+                Sociopedia
+            </Typography>
+            {isNonMobileScreens && (
+                <FlexBetween backgroundColor={neutralLight} borderRadius="9px" gap="3rem" padding="0.1rem 1.5rem" >
+                    <InputBase placeholder="Search..." />
+                    <IconButton>
+                        <Search />
+                    </IconButton>
+                </FlexBetween>
+            )}
+        </FlexBetween>
+        {/*DESKTOP NAV*/}
+        {isNonMobileScreens ? (<FlexBetween></FlexBetween>) : (<IconButton></IconButton>)}
+    </FlexBetween>
 }
 
 export default Navbar;
