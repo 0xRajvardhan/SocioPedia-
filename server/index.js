23a3337746186c279b1937bf03d4d906406486ae
Rajvardhan <singhrajvardhan2003@gmail.com>
2023-01-30 13:52:04 +0530

    fixing

--- server/index.js
+++ server/index.js
@@ -41,7 +41,7 @@ const storage = multer.diskStorage({
         cb(null, "public/assets");
     },
     filename: function (req, file, cb) {
-        cb(null, originalname);
+        cb(null, file.originalname);
     }
 });
 
