9580c5b3ae4083ec38514dd05dde981c80f5a0ea
Rajvardhan <singhrajvardhan2003@gmail.com>
2023-02-10 22:57:14 +0530

    add posts section

--- client/src/components/FlexBetween.jsx
+++ client/src/components/FlexBetween.jsx
@@ -5,6 +5,6 @@ const FlexBetween = styled(Box)({
     display: "flex",
     justifyContent: "space-between",
     alignItems: "center"
-})
+});
 
 export default FlexBetween;
--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -2,6 +2,7 @@ import { Box, useMediaQuery } from "@mui/material";
 import { useSelector } from "react-redux";
 import Navbar from "scenes/navbar";
 import UserWidget from "scenes/widgets/UserWidget";
+import MyPostWidget from "scenes/widgets/MyPostWidget";
 
 const HomePage = () => {
     const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
@@ -24,10 +25,11 @@ const HomePage = () => {
                     flexBasis={isNonMobileScreens ? "42%" : undefined}
                     mt={isNonMobileScreens ? undefined : "2rem"}
                 >
+                    <MyPostWidget picturePath={picturePath} />
                 </Box>
                 {isNonMobileScreens && (
                     <Box flexBasis="26%">
-    
+
                     </Box>
                 )}
             </Box>
